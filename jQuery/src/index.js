0f6781567878fe61cd3572adb0df57b22ee50848
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-05 14:31:17 +0400

    Add jQuery & remove ASP.NET Core

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,48 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
-    },
-  });
+  $('#like').dxButton({
+    icon: 'like',
+    stylingMode: 'outlined',
+    type: 'normal',
+  })
+    
+  $('#like-tooltip').dxTooltip({
+    target: '#like',
+    showEvent: 'mouseenter',
+    hideEvent: 'mouseleave',
+    contentTemplate: function(data) {
+      return $('<div>').text('Like')
+    }
+  })
+  
+  $('#trash').dxButton({
+    icon: 'trash',
+    stylingMode: 'outlined',
+    type: 'normal',
+  })
+  
+  $('#trash-tooltip').dxTooltip({
+    target: '#trash',
+    showEvent: 'mouseenter',
+    hideEvent: 'mouseleave',
+    contentTemplate: function(data) {
+      data[0].parentElement.classList.add('red-popup')
+      return $('<div>').text('Delete')
+    }
+  })
+
+  $('#info').dxButton({
+    icon: 'info',
+    stylingMode: 'outlined',
+    type: 'normal',
+  })
+  
+  $('#info-tooltip').dxTooltip({
+    target: '#info',
+    showEvent: 'mouseenter',
+    hideEvent: 'mouseleave',
+    contentTemplate: function(data) {
+      data[0].parentElement.classList.add('blue-popup')
+      return $('<div>').text('Info')
+    }
+  })
 });
