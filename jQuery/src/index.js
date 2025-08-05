a9656a5139deb456d4c825fe3ee1f6cdb42d6a43
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-05 14:35:25 +0400

    Fix linting

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -3,46 +3,46 @@ $(() => {
     icon: 'like',
     stylingMode: 'outlined',
     type: 'normal',
-  })
-    
+  });
+
   $('#like-tooltip').dxTooltip({
     target: '#like',
     showEvent: 'mouseenter',
     hideEvent: 'mouseleave',
-    contentTemplate: function(data) {
-      return $('<div>').text('Like')
-    }
-  })
-  
+    contentTemplate(data) {
+      return $('<div>').text('Like');
+    },
+  });
+
   $('#trash').dxButton({
     icon: 'trash',
     stylingMode: 'outlined',
     type: 'normal',
-  })
-  
+  });
+
   $('#trash-tooltip').dxTooltip({
     target: '#trash',
     showEvent: 'mouseenter',
     hideEvent: 'mouseleave',
-    contentTemplate: function(data) {
-      data[0].parentElement.classList.add('red-popup')
-      return $('<div>').text('Delete')
-    }
-  })
+    contentTemplate(data) {
+      data[0].parentElement.classList.add('red-popup');
+      return $('<div>').text('Delete');
+    },
+  });
 
   $('#info').dxButton({
     icon: 'info',
     stylingMode: 'outlined',
     type: 'normal',
-  })
-  
+  });
+
   $('#info-tooltip').dxTooltip({
     target: '#info',
     showEvent: 'mouseenter',
     hideEvent: 'mouseleave',
-    contentTemplate: function(data) {
-      data[0].parentElement.classList.add('blue-popup')
-      return $('<div>').text('Info')
-    }
-  })
+    contentTemplate(data) {
+      data[0].parentElement.classList.add('blue-popup');
+      return $('<div>').text('Info');
+    },
+  });
 });
