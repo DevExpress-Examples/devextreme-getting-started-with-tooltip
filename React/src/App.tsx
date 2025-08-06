772cf2d3cf768d0409db8f1b8613d7b71dfcf583
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-06 10:20:51 +0400

    Add React

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,16 +1,61 @@
-import { useCallback, useState } from 'react';
 import './App.css';
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import 'devextreme/dist/css/dx.fluent.blue.light.css';
+import { Button } from 'devextreme-react/button';
+import { Tooltip } from 'devextreme-react/tooltip';
+
+function likeRender(): JSX.Element {
+  return <div>Like</div>;
+}
+
+function trashRender(): JSX.Element {
+  return (
+    <div className='red-tooltip'>Delete</div>
+  );
+}
+
+function infoRender(): JSX.Element {
+  return (
+    <div className='blue-tooltip'>Info</div>
+  );
+}
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+    <div className="dx-viewport">
+      <div className="buttons">
+        <Button
+          id="like"
+          icon="like"
+        />
+        <Button
+          id="trash"
+          icon="trash"
+        />
+        <Button
+          id="info"
+          icon="info"
+        />
+      </div>
+      <div className="tooltips">
+        <Tooltip
+          target="#like"
+          showEvent="mouseenter"
+          hideEvent="mouseleave"
+          contentRender={likeRender}
+        />
+        <Tooltip
+          target="#trash"
+          showEvent="mouseenter"
+          hideEvent="mouseleave"
+          contentRender={trashRender}
+        />
+        <Tooltip
+          target="#info"
+          showEvent="mouseenter"
+          hideEvent="mouseleave"
+          contentRender={infoRender}
+        />
+      </div>
     </div>
   );
 }
