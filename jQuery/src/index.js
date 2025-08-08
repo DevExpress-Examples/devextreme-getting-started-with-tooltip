fa2119a6cb04ad0810541d20b02d81ff26037c88
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2025-08-08 11:15:29 +0400

    Add jQuery & remove ASP.NET Core (#1)

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,50 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  $('#like').dxButton({
+    icon: 'like',
+    stylingMode: 'outlined',
+    type: 'normal',
+  });
+
+  $('#like-tooltip').dxTooltip({
+    target: '#like',
+    showEvent: 'mouseenter',
+    hideEvent: 'mouseleave',
+    contentTemplate() {
+      return $('<div>').text('Like');
+    },
+  });
+
+  $('#trash').dxButton({
+    icon: 'trash',
+    stylingMode: 'outlined',
+    type: 'normal',
+  });
+
+  $('#trash-tooltip').dxTooltip({
+    target: '#trash',
+    showEvent: 'mouseenter',
+    hideEvent: 'mouseleave',
+    contentTemplate() {
+      return $('<div>')
+        .addClass('red-tooltip')
+        .text('Delete');
+    },
+  });
+
+  $('#info').dxButton({
+    icon: 'info',
+    stylingMode: 'outlined',
+    type: 'normal',
+  });
+
+  $('#info-tooltip').dxTooltip({
+    target: '#info',
+    showEvent: 'mouseenter',
+    hideEvent: 'mouseleave',
+    contentTemplate() {
+      return $('<div>')
+        .addClass('blue-tooltip')
+        .text('Info');
     },
   });
 });
